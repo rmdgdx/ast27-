bc2f85152950499f409508cf7e2ca6872fd55250
rmgdx <115793066+rmdgdx@users.noreply.github.com>
2026-08-17 19:07:45 +0800

    Add files via upload

---
+++ ast27-1786964786970.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit: Gen Z SciComm Interactive Chapter"
+git branch -M main
+git remote add origin https://github.com/YOUR-USERNAME/YOUR-REPO-NAME.git
+git push -u origin main
