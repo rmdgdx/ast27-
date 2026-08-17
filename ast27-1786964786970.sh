1f978aef7b6e278e07acd9fad4f30e550ec65f9a
rmgdx <115793066+rmdgdx@users.noreply.github.com>
2026-08-17 19:10:18 +0800

    Delete ast27-1786964786970.sh

--- ast27-1786964786970.sh
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit: Gen Z SciComm Interactive Chapter"
-git branch -M main
-git remote add origin https://github.com/YOUR-USERNAME/YOUR-REPO-NAME.git
-git push -u origin main
